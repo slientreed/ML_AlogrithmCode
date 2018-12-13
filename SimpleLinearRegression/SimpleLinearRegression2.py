74aaadbe13f0edc95a0a4153d626f353e288cb28
Mark <1817956299@qq.com>
2018-12-13 17:02:12 +0800

    SimpleLinearRegresssion

---
+++ SimpleLinearRegression/SimpleLinearRegression2.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+'''使用向量计算a,b参数的方式'''
+class SimpleLinearRegression2:
+
+    def __init__(self):
+        """初始化Simple Linear Regression 模型"""
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        """根据训练数据集x_train,y_train训练Simple Linear Regression模型"""
+        assert x_train.ndim == 1
+        assert len(x_train) == len(y_train)
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        '''这里使用向量化，采用numpy中的向量相乘计算a,b值'''
+        self.a_ = (x_train - x_mean).dot(y_train - y_mean) / (x_train - x_mean).dot(x_train - x_mean)
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        """给定待预测数据集x_predict，返回表示x_predict的结果向量"""
+        assert x_predict.ndim == 1
+        assert self.a_ is not None and self.b_ is not None
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        """给定单个待预测数据x，返回x的预测结果值"""
+        return self.a_ * x_single + self.b_
+
+    def __repr__(self):
+        return "SimpleLinearRegression2()"
